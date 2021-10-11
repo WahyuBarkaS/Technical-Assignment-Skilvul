944affd1d550434c6d8ca0483315c3f0e2f58cae
WahyuBarkaS <56128906+WahyuBarkaS@users.noreply.github.com>
2021-10-11 14:47:59 +0800

    Update and rename array.js to 01-array.js

---
+++ JS-Intermediate/01-array.js
@@ -0,0 +1,93 @@
+// Soal - 01
+
+const arr = [1, 2, 3];
+const arr2 = [8, 1, 5, 7];
+
+function reverse(number) {
+    let newArr = [];     
+
+    if (number == arr) {
+        arr.forEach(num => {
+            newArr.unshift(num)
+        });
+        return newArr;
+    }
+    else {
+        arr2.forEach(num => {
+            newArr.unshift(num)
+        });
+        return newArr;
+    }
+}
+
+const newArr = reverse(arr);
+const newArr2 = reverse(arr2);
+
+console.log(arr, newArr);
+console.log(arr2, newArr2);
+
+// Soal - 02
+const arr1 = [1, 3, 4, 1, 2, 8];
+const arr2 = [5, 6, 7, 8, 1, 3];
+
+function getAverage(number) {
+    let output = [];  
+    let sum = 0; 
+    let count = number.length; 
+    let averageNumber = 0; 
+
+    if (number == arr1) {
+        arr1.forEach((item) => {
+            sum += item
+        });
+        averageNumber = (sum/count).toFixed(0);
+        averageNumber = Number(averageNumber); 
+
+        for (let i = 0; i < number.length; i++){
+            if (number[i] > averageNumber) {
+                output.push(arr1[i])
+            };
+        };
+    return output.length;
+    }
+    else if (number == arr2) {
+        arr2.forEach((item) => {
+            sum += item
+        });
+        averageNumber = (sum/count).toFixed(0)
+        averageNumber = Number(averageNumber)
+        for (let i = 0; i < number.length; i++){
+            if (number[i] > averageNumber) {
+                output.push(arr2[i])
+            };
+        };
+    return output.length
+    };
+};
+console.log(getAverage(arr1))
+console.log(getAverage(arr2))
+
+// Soal - 03
+const arr = [
+    [10],
+    [9, 7, 1],
+    [2, 8],
+  ];
+  
+function searchInArray(arr, num) {
+    let convArr = [] 
+    convArr = arr.join().split(","); 
+    convArrNum = convArr.map(i=>Number(i)); 
+
+    for (let i = 0; i < convArrNum.length; i++) {
+        if(convArrNum[i] == num){
+            return i  
+        }
+    }
+    return null
+}
+
+console.log(searchInArray(arr, 3));
+console.log(searchInArray(arr, 2));
+console.log(searchInArray(arr, 4));
+console.log(searchInArray(arr, 8));
